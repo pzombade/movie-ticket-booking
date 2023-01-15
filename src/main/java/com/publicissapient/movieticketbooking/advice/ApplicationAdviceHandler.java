0de4fabfc2f1da393b132c448790d456c1c4fb8b
pzombade <prashantzombade@gmail.com>
2023-01-16 01:48:27 +0530

    User password is encrpypted, add login API

---
+++ src/main/java/com/publicissapient/movieticketbooking/advice/ApplicationAdviceHandler.java
@@ -0,0 +1,19 @@
+package com.publicissapient.movieticketbooking.advice;
+
+import com.publicissapient.movieticketbooking.exception.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ApplicationAdviceHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> userNotFound(String msg){
+        log.error("UserNotFoundException: "+ msg);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+    }
+}
